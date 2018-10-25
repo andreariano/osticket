4cf4f502001e1fad1b01bfa7a17389f6c3ea2968
Andre Ariano <andreariano@yahoo.com.br>
2018-10-25 14:41:06 -0300

    Update to Connection String

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','osticketsrvariano01.mysql.database.azure.com');
+define('DBHOST','osticketariano.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@osticketsrvariano01');
+define('DBUSER','demouser@osticketariano');
 define('DBPASS','demo@pass123');
 
 # Table prefix
